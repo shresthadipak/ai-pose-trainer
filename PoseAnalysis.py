2f9f0c1452f6d6662355a495915414523dd111bc
Dipak <shresthadipak2049@gmail.com>
2023-01-16 19:24:33 +0545

    Feat:added the evalution methods for shoulder press

--- PoseAnalysis.py
+++ PoseAnalysis.py
@@ -136,4 +136,77 @@ class poseEvaluate():
 
         return angle_el, angle_sl, present    
 
-        
+    def _shoulder_press(self, image, landmarks, mp_pose, color=None, draw=True):
+        self.h, self. w, _ = image.shape
+        # Get the coordinates of Right part
+        self.rhip = [landmarks[mp_pose.RIGHT_HIP.value].x, landmarks[mp_pose.RIGHT_HIP.value].y]
+        self.rshoulder = [landmarks[mp_pose.RIGHT_SHOULDER.value].x, landmarks[mp_pose.RIGHT_SHOULDER.value].y]
+        self.relbow = [landmarks[mp_pose.RIGHT_ELBOW.value].x, landmarks[mp_pose.RIGHT_ELBOW.value].y]
+        self.rwrist = [landmarks[mp_pose.RIGHT_WRIST.value].x, landmarks[mp_pose.RIGHT_WRIST.value].y]
+
+        # Get the coordinates of left part
+        self.lhip = [landmarks[mp_pose.LEFT_HIP.value].x, landmarks[mp_pose.LEFT_HIP.value].y]
+        self.lshoulder = [landmarks[mp_pose.LEFT_SHOULDER.value].x, landmarks[mp_pose.LEFT_SHOULDER.value].y]
+        self.lelbow = [landmarks[mp_pose.LEFT_ELBOW.value].x, landmarks[mp_pose.LEFT_ELBOW.value].y]
+        self.lwrist = [landmarks[mp_pose.LEFT_WRIST.value].x, landmarks[mp_pose.LEFT_WRIST.value].y]
+
+        # angle left part
+        angle_el = calculateAngle.angleFinder(self.lshoulder, self.lelbow, self.lwrist)
+        angle_sl = calculateAngle.angleFinder(self.lhip, self.lshoulder, self.lelbow)
+
+        # angle right part
+        angle_er = calculateAngle.angleFinder(self.rshoulder, self.relbow, self.rwrist)
+        angle_sr = calculateAngle.angleFinder(self.rhip, self.rshoulder, self.relbow)
+
+
+        # Visibilty Check
+
+        # Visiblity Check of left part
+        self.lHip_conf = landmarks[mp_pose.LEFT_HIP.value].visibility
+        self.lShoulder_conf = landmarks[mp_pose.LEFT_SHOULDER.value].visibility
+        self.lElbow_conf = landmarks[mp_pose.LEFT_ELBOW.value].visibility
+        self.lWrist_conf = landmarks[mp_pose.LEFT_WRIST.value].visibility
+
+        # Visiblity Check of right part
+        self.rHip_conf = landmarks[mp_pose.RIGHT_HIP.value].visibility
+        self.rShoulder_conf = landmarks[mp_pose.RIGHT_SHOULDER.value].visibility
+        self.rElbow_conf = landmarks[mp_pose.RIGHT_ELBOW.value].visibility
+        self.rWrist_conf = landmarks[mp_pose.RIGHT_WRIST.value].visibility
+
+        present = 0
+        if self.rHip_conf > 0.1 and  self.rShoulder_conf > 0.1 and  self.rElbow_conf > 0.1 and self.rWrist_conf > 0.1 and self.lHip_conf > 0.1 and  self.lShoulder_conf > 0.1 and  self.lElbow_conf > 0.1 and self.lWrist_conf > 0.1:
+            present = 1 
+
+
+        # Draw the keypoints
+        if color:
+            color = color
+        else:
+            color = (255, 0, 255)
+
+        if draw:
+            # Right part
+            cv2.circle(image, tuple(np.multiply(self.relbow, [self.w, self.h]).astype(int)), 3, color, cv2.FILLED)
+            cv2.circle(image, tuple(np.multiply(self.relbow, [self.w, self.h]).astype(int)), 15, color, 2)
+            cv2.circle(image, tuple(np.multiply(self.rshoulder, [self.w, self.h]).astype(int)), 3, color, cv2.FILLED)
+            cv2.circle(image, tuple(np.multiply(self.rshoulder, [self.w, self.h]).astype(int)), 15, color, 2)
+            cv2.circle(image, tuple(np.multiply(self.rwrist, [self.w, self.h]).astype(int)), 3, color, cv2.FILLED)
+            cv2.circle(image, tuple(np.multiply(self.rwrist, [self.w, self.h]).astype(int)), 15, color, 2)
+            cv2.circle(image, tuple(np.multiply(self.rhip, [self.w, self.h]).astype(int)), 3, color, cv2.FILLED)
+            cv2.circle(image, tuple(np.multiply(self.rhip, [self.w, self.h]).astype(int)), 15, color, 2)
+
+            # Left part
+            cv2.circle(image, tuple(np.multiply(self.lelbow, [self.w, self.h]).astype(int)), 3, color, cv2.FILLED)
+            cv2.circle(image, tuple(np.multiply(self.lelbow, [self.w, self.h]).astype(int)), 15, color, 2)
+            cv2.circle(image, tuple(np.multiply(self.lshoulder, [self.w, self.h]).astype(int)), 3, color, cv2.FILLED)
+            cv2.circle(image, tuple(np.multiply(self.lshoulder, [self.w, self.h]).astype(int)), 15, color, 2)
+            cv2.circle(image, tuple(np.multiply(self.lwrist, [self.w, self.h]).astype(int)), 3, color, cv2.FILLED)
+            cv2.circle(image, tuple(np.multiply(self.lwrist, [self.w, self.h]).astype(int)), 15, color, 2)
+            cv2.circle(image, tuple(np.multiply(self.lhip, [self.w, self.h]).astype(int)), 3, color, cv2.FILLED)
+            cv2.circle(image, tuple(np.multiply(self.lhip, [self.w, self.h]).astype(int)), 15, color, 2)
+
+            # cv2.putText(image, str(int(angle_er)), tuple(np.multiply(self.relbow, [self.w-200, self.h]).astype(int)), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+            # cv2.putText(image, str(int(angle_sr)), tuple(np.multiply(self.rshoulder, [self.w-200, self.h]).astype(int)), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+
+        return  angle_er, angle_sr, angle_el, angle_sl, present   
+    
